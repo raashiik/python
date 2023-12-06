b18c2e5dafee696c5105b97a920c6201cb168418
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:47:51 +0530

    for1.py

---
+++ for1.py
@@ -0,0 +1,19 @@
+flowers=['rose','lotus','jasmine']
+for item in flowers:
+    print('current item:',item)
+
+print('\n num set......................................\n')
+list=[6,5,3,8,4,2,5,4,11]
+sum=0
+for item in list:
+    sum=sum+item
+print("The sum is:",sum)
+
+
+
+print('\n change of print position......................................\n')
+list=[6,5,3,8,4,2,5,4,11]
+sum=0
+for item in list:
+    sum=sum+item
+    print("The sum is:",sum)
