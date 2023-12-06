9034f2e797078b238eebb9496765232dd660ec42
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:59:17 +0530

    set.py

---
+++ set.py
@@ -0,0 +1,31 @@
+print('length of a set \n')
+set3={1000,'manu',5,700,15.9,7000,100,}
+print(len(set3))
+
+set1={1000,5,700,15.9,7000,100,}
+
+print('max of a set \n')
+print(max(set1))
+
+print('Min of a set \n')
+print(min(set1))
+
+print('sum of set1 \n')
+print(sum(set1))
+
+print('sort of set1 \n')
+print(set1)
+set2=sorted(set1)
+print(set2)
+
+
+
+print("Another set ;;;;;;;;;;;;;;;\n")
+
+print('Enumerate:\n')
+grocery=['bread','milk','butter']
+enum1 =enumerate(grocery)
+print(enum1)
+
+print(list(enum1))
+print(tuple(enum1))
