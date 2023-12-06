6160baca95fe88aa6d3bb1829e187036c7558461
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 12:01:20 +0530

    add_ing.py

---
+++ add_ing.py
@@ -0,0 +1,10 @@
+print("Add 'ing' at the end of a given string.If it already ends with 'ing' ,then add 'ly'  \n")
+
+def modifyStr(str1):
+    if str1.endswith("ing"):
+        return str1+"ly"
+    else:
+        return str1+"ing"
+str1=input("Enter a string:")
+result=modifyStr(str1)
+print(result)
