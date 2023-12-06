5b3edb9633868c3a1ecd1bc30aa5d6c49d4033a6
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:56:24 +0530

    numvariables.py

---
+++ numvariables.py
@@ -0,0 +1,10 @@
+print("Number variables")
+regno=1000
+total=800
+print("Register number:"+str(regno)+"\nTotal:"+str(total))
+
+print("same program use another method:")
+
+REGISTERNUM=2000
+TOTAL=700
+print(f"Register number:{REGISTERNUM}\n Total:{TOTAL}")
