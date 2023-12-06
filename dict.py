c60e9fc9014ecc77e92315d611861d88f64b009e
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:47:08 +0530

    dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+a=("RegNo : ","Name : ","Total : ")
+b=(11111 ,"Christy",450)
+tuple1= zip(a,b)
+print("Tuple1 : ",tuple(tuple1))
+
+list1=zip(a,b)
+print("list1 : ",list(list1))
+
+dict1=zip(a,b)
+print("dict1 : ",dict(dict1))
+
