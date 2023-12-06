b7b3792cf856873c08a5097fc874c7e8222d8fac
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:57:57 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,13 @@
+Python 3.8.10 (default, May 26 2023, 14:05:08) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n1=input("Enter first number:")
+Enter first number:7
+>>> n2=input("Enter second number:")
+Enter second number:3
+>>> n3=input("Enter third number:")
+Enter third number:5
+>>> print("The entered numbers are :\n",n1,n2,n3)
+The entered numbers are :
+ 7 3 5
+>>> 
