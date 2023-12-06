e26ff9c4febaac99790ceb4556c4180c78b59ec0
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:52:44 +0530

    list1.py

---
+++ list1.py
@@ -0,0 +1,29 @@
+a=[1,2.2,'python']
+print("a=",a)
+print("a=" ,a[0])
+print(len(a))
+
+
+
+
+print("\n A list of elements  \n \n")
+b=[5,10,15,20,25,30,35,40]
+print("b=",b)
+print("b[2]=",b[2])
+
+"""
+print("b[0.3]=",b[0.3])
+print("b[5:]=",b[5:])  """
+
+
+print(" \n \n Build in list functions: \n \n")
+
+list=[1000,'Pravii',600,1800]
+print("length:",len(list))
+
+print("Maximum:",max(list))
+
+print("Minimum:",min(list))
+
+
+
