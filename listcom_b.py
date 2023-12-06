cf17deef87a5c85aad23f762f92f0ff47c7eb4b8
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:54:18 +0530

    Create listcom_b.py

---
+++ listcom_b.py
@@ -0,0 +1,9 @@
+
+
+
+print('\n  List Comprehension. SUARE OF N NUMBERS...................................... \n ')
+list1=[1,2,3,4,5,6] #code shorthanded
+list2=[i**2 for i in list1]
+
+
+print(f"positive list ={list2}")
