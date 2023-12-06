af96bb4ad4b2cda06497e63d5c983a3fa79be9b6
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 11:59:38 +0530

    sum.py

---
+++ sum.py
@@ -0,0 +1,18 @@
+print("Program for Add two numbers")
+num1=int(input("Enter one number n1:"))
+num2=int(input("Enter one number n2:"))
+sum=num1+num2
+print("The sum two entered num is sum :",sum)
+
+
+sub=num1-num2
+print(f"difference is:{sub}")
+
+
+
+mul=num1*num2
+print(f"multiple is:{mul}")
+
+
+div=num1/num2
+print(f"quotient is:{div}")
