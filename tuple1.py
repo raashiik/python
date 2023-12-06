c9aef226d29c424aaedf447674f48fc0111309bc
Rashik <145551063+raashiik@users.noreply.github.com>
2023-12-06 12:00:08 +0530

    tuple1.py

---
+++ tuple1.py
@@ -0,0 +1,20 @@
+"""a=(10,12.6,'pravii')
+print('a=',a)
+b=(190)
+print(b)"""
+
+
+print(' \n  set \n')
+a={5,3,6}
+print("a=",a)
+c={5,3,3,7,2,2,2}
+print(" \n REPEATATION NOT ALLOWED ")
+print("c=",c)
+
+
+
+
+print(' \n DICTIONARY KEY WITH ITS VALUE: \n')
+
+dic={'NAME ':'Pravii', 'AGE' :17,'CLASS':12}
+print("dict")
